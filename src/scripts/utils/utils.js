0f24348fd667ab14ebd4de65e31c54914e5de19f
brendan.lee.reinhart <brendan.lee.reinhart@accenture.com>
2018-04-13 16:26:33 +1000

    Added difficulty and category identifiers. Added visual distinction between answers for selection. Added number selector in prep for form

--- src/scripts/utils/utils.js
+++ src/scripts/utils/utils.js
@@ -1,112 +1,62 @@
-var test = 'test';
-var dog = 'dog';
-var cat = 'cat';
-var wonton = 'wonton';
-var tandy = 'tandy';
-var ghost = 'ghost is here. woo. Yeah.';
-function testUtil(){
-    console.log('Utils file loaded.')
-    return 2;
-}
-testUtil();
 
+function returnQuestions(response) { // use an each to loop over questions and generate html
+    $.each(response.results, function(index, result) {
+        
+        // dynamically define <li> for each question:
+        var trivQuestion = $('<li class="question"></li>');
+        if (result.difficulty == "hard") {
+            trivQuestion.addClass('hard')
+        } else if(result.difficulty == "medium") {
+            trivQuestion.addClass('medium')
+        } else {
+            trivQuestion.addClass('easy')
+        }
+        $('<p class="category-identifier"><i>'+result.category+'</i></p>').appendTo(trivQuestion);
+        $('<p class="question-text"><b>'+result.question+'</b></p>').appendTo(trivQuestion);
 
+        // generate <ul> for answers:
+        var answerList = $('<ul class="answer-list"></ul>');
+        // randomise answer order:
+        // TO DO: handle true/false case.
+        var answerArray = [
+        $('<li class="correct-answer"><b>'+result.correct_answer+'</b></li>'),
+        $('<li class="incorrect-answer"><b>'+result.incorrect_answers[0]+'</b></li>'),
+        $('<li class="incorrect-answer"><b>'+result.incorrect_answers[1]+'</b></li>'),
+        $('<li class="incorrect-answer"><b>'+result.incorrect_answers[2]+'</b></li>')
+        ];
+        answerArray.sort(function() { return 0.5 - Math.random() }); 
+        for(i=0;i<answerArray.length;i++) {
+            answerArray[i].appendTo(answerList);
+        }
 
-$('.get-questions').on('click', function() {
-    $('.question-list').html('');
-    console.log('CLICKED');
-    // Move AJAX call to own functions --> getQuestions()
-    // getQuestions();
+        // append answers list <ul> to the original question <li> item
+        answerList.appendTo(trivQuestion);
     
-    $.ajax('https://opentdb.com/api.php', {
-        data: { "amount": 10 },
-        success: function(response) { // use an each to loop over questions and generate html
-            $.each(response.results, function(index, result) {
-                console.log(result);
-                console.log('question: '+result.question);
-                console.log('correct answer: '+result.correct_answer);
-                console.log('INcorrect answers: '+result.incorrect_answers);
-                var trivQuestion = $('<li></li>');
-                $('<p><b>'+result.question+'</b></p>').appendTo(trivQuestion);
+        // append question <li> item to <ul> .question-list
+        trivQuestion.appendTo('.question-list');
+    });
+    
+};
 
-                // define <ol> and populate with possible answers
-                var answerList = $('<ul class="answer-list"></ul>');
-                // function randNum() {return Math.floor((Math.random()*100))};
-                // randNumList = [randNum(), randNum(), randNum(), randNum()];
-
-                var answerArray = [
-                $('<li class="correct-answer"><b>'+result.correct_answer+'</b></li>'),
-                $('<li class="incorrect-answer"><b>'+result.incorrect_answers[0]+'</b></li>'),
-                $('<li class="incorrect-answer"><b>'+result.incorrect_answers[1]+'</b></li>'),
-                $('<li class="incorrect-answer"><b>'+result.incorrect_answers[2]+'</b></li>')
-                ];
-                answerArray.sort(function() { return 0.5 - Math.random() }); // randomise answer order
-                for(i=0;i<answerArray.length;i++) {
-                    answerArray[i].appendTo(answerList);
-                }
-
-                // append answers list to the original <li> item
-                answerList.appendTo(trivQuestion);
-               
-                // append <li> item to <ul> .question-list
-                trivQuestion.appendTo('.question-list');
-            });
-            
-        },
+function getQuestions(options) {
+    var params = $.extend({ "amount": 10 }, options);
+    return $.ajax('https://opentdb.com/api.php', {
+        data: params,
+        success: returnQuestions,
         error: function(request, errorType, errorMessage) {
             alert('Error: ' + errorType + ' with message: ' + errorMessage);
         }
-    });
-    $('.question-list').fadeIn();
-})
-
-$('.get-questions').on('click', function() {
-    $('.question-list').html('');
-    console.log('CLICKED');
-    $.ajax('https://opentdb.com/api.php', {
-        data: { "amount": 10 },
-        success: function(response) { // use an each to loop over questions and generate html
-            $.each(response.results, function(index, result) {
-                console.log(result);
-                console.log('question: '+result.question);
-                console.log('correct answer: '+result.correct_answer);
-                console.log('INcorrect answers: '+result.incorrect_answers);
-                var trivQuestion = $('<li></li>');
-                $('<p><b>'+result.question+'</b></p>').appendTo(trivQuestion);
-
-                // define <ol> and populate with possible answers
-                var answerList = $('<ul class="answer-list"></ul>');
-                // function randNum() {return Math.floor((Math.random()*100))};
-                // randNumList = [randNum(), randNum(), randNum(), randNum()];
-
-                var answerArray = [
-                $('<li class="correct-answer"><b>'+result.correct_answer+'</b></li>'),
-                $('<li class="incorrect-answer"><b>'+result.incorrect_answers[0]+'</b></li>'),
-                $('<li class="incorrect-answer"><b>'+result.incorrect_answers[1]+'</b></li>'),
-                $('<li class="incorrect-answer"><b>'+result.incorrect_answers[2]+'</b></li>')
-                ];
-                answerArray.sort(function() { return 0.5 - Math.random() }); // randomise answer order
-                for(i=0;i<answerArray.length;i++) {
-                    answerArray[i].appendTo(answerList);
-                }
-
-                // append answers list to the original <li> item
-                answerList.appendTo(trivQuestion);
-               
-                // append <li> item to <ul> .question-list
-                trivQuestion.appendTo('.question-list');
-            });
-            
-        },
-        error: function(request, errorType, errorMessage) {
-            alert('Error: ' + errorType + ' with message: ' + errorMessage);
-        }
-    });
-    $('.question-list').fadeIn();
-})
+    }
+)};
 
 // ~~~~~~~~~~ EVENT HANDLERS ~~~~~~~~~~
 
+$('.get-questions').on('click', function() {
+    $('.question-list').html('');
+    // Move AJAX call to own functions --> getQuestions()
+    getQuestions({"amount": 4});
+    $('.question-list').fadeIn();
+})
 
 
 /* event handler for the pressing of the answers.
